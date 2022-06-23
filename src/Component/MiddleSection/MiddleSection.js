34ba579d42f6610b2bc5a7182c5340eabd250105
arknahian <mr.asfo518@gmail.com>
2022-06-23 21:03:55 +0600

    another try

--- src/Component/MiddleSection/MiddleSection.js
+++ src/Component/MiddleSection/MiddleSection.js
@@ -15,7 +15,7 @@ const MiddleSection = () => {
             This is trending nfts
             There will be some cards
             this is another card
-            this
+            this is
 
         </div>
     );
